4be3c1483cce942bc31a1edfe39199363488219c
chengkangzai <pycck@hotmail.com>
2021-02-08 23:51:14 +0800

    improve firebase import and add timestamp to Food

--- WhatToEat/src/app/model/food.ts
+++ WhatToEat/src/app/model/food.ts
@@ -1,8 +1,12 @@
+import firebase from 'firebase/app';
+import Timestamp = firebase.firestore.Timestamp;
+
 export class Food {
     constructor(
         public id: string,
         public food: string,
         public userID: string,
+        public timestamp?: Timestamp, // temporary, may remove when all user food have timestamp
     ) {
     }
 }
