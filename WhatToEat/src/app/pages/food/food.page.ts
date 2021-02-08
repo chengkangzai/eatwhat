a93f7f111030a4a45b1fdf9a833b4e4f3225c788
chengkangzai <pycck@hotmail.com>
2021-02-08 23:52:17 +0800

    improve validation of add food

--- WhatToEat/src/app/pages/food/food.page.ts
+++ WhatToEat/src/app/pages/food/food.page.ts
@@ -42,7 +42,7 @@ export class FoodPage implements OnInit {
 
         this.form = new FormGroup({
             food: new FormControl(null, {
-                updateOn: 'blur',
+                updateOn: 'change',
                 validators: [Validators.required]
             })
         });
